28b777f9da6f4ae328358301fe5bb08c89384faa
jcodling <circuitjc@gmail.com>
2014-01-08 14:21:28 -0500

    Fixed JSON
    Fixed the format of the JSON data for the contacts.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,32 @@
 /* Controllers */
 
 angular.module('contactListApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('ContactListCtrl', function($scope) {
+  
+  	$scope.contacts [
+		{
+			"first": "Jim",
+			"last": "Cooper",
+			"extention": "3219"
+		},
+		{
+			"first": "Liane",
+			"last": "Cooper",
+			"extention": "3418"
+		},
+		{
+			"firste": "Rick",
+			"last": "Brown",
+			"extention": "3376"
+		},
+		{
+			"first": "Len",
+			"last": "Payne",
+			"extention": "3418"
+		}
+	]
 
-  }])
+  })
   .controller('MyCtrl2', [function() {
 
   }]);

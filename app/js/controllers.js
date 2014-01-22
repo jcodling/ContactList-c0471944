4e435ae2ec69dbd8435a3d62950de18a23e88c9a
jcodling <circuitjc@gmail.com>
2014-01-22 10:04:43 -0500

    First two steps.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,28 +3,10 @@
 /* Controllers */
 
 angular.module('contactListApp.controllers', []).
-  controller('ContactListCtrl', function($scope) {
-  
-  	$scope.contacts = [
-		{
-			"first": "Jim",
-			"last": "Cooper",
-			"extension": "3219"
-		},
-		{
-			"first": "Liane",
-			"last": "Cooper",
-			"extension": "3336"
-		},
-		{
-			"first": "Rick",
-			"last": "Brown",
-			"extension": "3376"
-		},
-		{
-			"first": "Len",
-			"last": "Payne",
-			"extension": "3418"
-		}
-	];
-  });
+	controller('ContactListCtrl', function($scope,$http) {
+	  $http.get('contacts/contacts.json').success(function(data){
+	    $scope.contacts = data;
+	  });
+	  
+	  $scope.orderProp = "first";
+});
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,45 +1,73 @@
 'use strict';
 
+/* Assignment #1 End-to-End Testing
+ * Prepared By: Len Payne
+ * For: CPD-3262
+ * 2014/01/07
+ */
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('Contact List App', function() {
 
   beforeEach(function() {
     browser().navigateTo('app/index.html');
   });
 
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
+  it('should have four tr elements with the class "contact"', function() {
+	expect(repeater('tr.contact').count()).toBe(4);
+  });
+  
+  it('should filter on Cooper to only two results', function() {
+	input('query').enter('Cooper');
+    expect(repeater('tr.contact').count()).toBe(2);
   });
 
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
-    });
-
+  it('should filter on o to only three results', function() {
+	input('query').enter('o');
+    expect(repeater('tr.contact').count()).toBe(3);
   });
 
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
+  it('should order correctly by Extension', function() {
+	input('query').enter('33');
+	select('orderProp').option('Extension');
+	expect(repeater('tr.contact', 'Contact List').column('contact.first')).
+          toEqual(["Liane", "Rick"]);
   });
+
+  it('should order correctly by First Name', function() {
+	input('query').enter('Cooper');
+	select('orderProp').option('First Name');
+	expect(repeater('tr.contact', 'Contact List').column('contact.first')).
+          toEqual(["Jim", "Liane"]);
+  });
+
+  it('should order correctly by Last Name', function() {
+	input('query').enter('L');
+	select('orderProp').option('Last Name');
+	expect(repeater('tr.contact', 'Contact List').column('contact.first')).
+          toEqual(["Liane", "Len"]);
+  });
+
+  it('should include all the original first names', function() {
+	select('orderProp').option('First Name');
+	expect(repeater('tr.contact', 'Contact List').column('contact.first')).
+          toEqual(["Jim", "Len", "Liane", "Rick"]);		
+  });
+  
+  it('should include all the original last names', function() {
+	select('orderProp').option('First Name');
+	expect(repeater('tr.contact', 'Contact List').column('contact.last')).
+          toEqual(["Cooper", "Payne", "Cooper", "Brown"]);		
+  });
+  
+  it('should correctly construct the E-Mail as "len.payne@lambtoncollege.ca"', function() {
+	input('query').enter('Len');
+	expect(element('tr.contact td.email').text()).toMatch(/^len\.payne@lambtoncollege\.ca$/i);          
+  });
+  
+  it('should correctly construct the Phone Number as "519-542-7751 ext. 3418"', function() {
+	input('query').enter('Len');
+	expect(element('tr.contact td.phone').text()).toMatch(/^519-542-7751 ext\. 3418$/i);          
+  });
+  
 });

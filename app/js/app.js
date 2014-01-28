e4077e394e20cd8bf28c3c978861949766ada0d8
jcodling <circuitjc@gmail.com>
2014-01-28 17:30:59 -0500

    Final Exercise #2

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ angular.module('contactListApp', [
   'contactListApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/contacts', {templateUrl: 'partials/contact-list.html', controller: 'ContactListCtrl'});
+  $routeProvider.when('/contacts/:contactId', {templateUrl: 'partials/contact-detail.html', controller: 'ContactDetailCtrl'});
+  $routeProvider.otherwise({redirectTo: '/contacts'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,21 @@
 
 /* Controllers */
 
-angular.module('contactListApp.controllers', []).
-	controller('ContactListCtrl', function($scope,$http) {
+var contactsControllers = angular.module('contactListApp.controllers', []);
+
+contactsControllers.controller('ContactListCtrl', function($scope,$http) {
 	  $http.get('contacts/contacts.json').success(function(data){
 	    $scope.contacts = data;
 	  });
 	  
 	  $scope.orderProp = "first";
 });
+
+contactsControllers.controller('ContactDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope,$routeParams,$http) {
+	  $http.get('contacts/' + $routeParams.contactId +'.json').success(function(data){
+	    $scope.contact = data;
+	  });
+	  
+	  $scope.orderProp = "first";
+}]);
